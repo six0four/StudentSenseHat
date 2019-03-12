0bfc4ada0f5d229decfc95e89771f27ac7a0c68d
six0four <mdrk0011@humber.ca>
2019-03-12 13:37:30 -0400

    Added opencm3

--- firmware/beginstmv01.sh
+++ firmware/beginstmv01.sh
@@ -14,5 +14,12 @@ gparted \
 xorg xorg-dev \
 xscreensaver -y
 git clone https://github.com/ve3wwg/stm32f103c8t6.git ~/stm32f103c8t6
+git clone https://github.com/libopencm3/libopencm3.git ~/stm32f103c8t6/libopencm3
+wget https://sourceforge.net/projects/freertos/files/FreeRTOS/V10.2.0/FreeRTOSv10.2.0.zip/download -d /home/pi/stm32f103c8t6/rtos/
+unzip ~/stm32f103c8t6/rtos/FreeRTOSv10.0.1.zip /pi/stm32f103c8t6/rtos/
+
 cd /opt
+
+cd ~/stm32f103c8t6
+make
 echo finished script
